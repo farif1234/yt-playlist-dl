a9188eb46ccb020cc26df262eb4340707296e3f2
Faihaan Arif <farif1234@gmail.com>
2024-07-14 10:36:46 -0700

    add gui boilerplate code

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # settings
 
-PLAYLIST_URL = ''
-PATH = r''
+PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PL_BSOiX8jRY-C9Q2fWdnw6Ze4bjEcyy3n'
+PATH = r'C:\Program Files\Image-Line\FL Studio 20\Data\Patches\USER\BEATS'
 RECORD_TEXT_PATH = PATH + r'\record.txt'
 RECORD_HTML_PATH = PATH + r'\record.html'
-DL_OUTPUT_PATH = PATH + r'\yt_downloads'
+DL_OUTPUT_PATH = PATH + r'\yt_downlads'
 FFMPEG_PATH = r'C:\ffmpeg\bin\ffmpeg.exe'
 ERROR_PATH = 'error.log'
-NUMBER_OF_DOWNLOADS = 10
+NUMBER_OF_DOWNLOADS = 300
